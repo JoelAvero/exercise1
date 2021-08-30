04fc6c04d331365e688c7867daeb31acce628259
Joel Avero <joelavero@gmail.com>
2021-08-30 17:35:20 -0300

    code was cleaned, some functions was refactored

--- src/components/register/Register.tsx
+++ src/components/register/Register.tsx
@@ -1,24 +1,29 @@
-import FormRegister from "./FormRegister"
-import "./styles.scss"
+import FormRegister from "./FormRegister";
+import "./styles.scss";
 
 const Register = () => {
-    return (
-        <div className="container">
-            <div className="content">
-                <div className="content-left">
-                    <div className="left-div">
-                        <h1 className="text1">Learn to code by watching others</h1>
-                        <p className="text2">See how experienced developers solve problems in real-time. Watching scripted
-                            tutorials is great, but understanding how developers think is invaluable.</p>
-                    </div>
-                </div>
-                <div className="content-right">
-                    <div className="try-it-free"><strong>Try it free 7 days</strong> then $20/mo. thereafter</div>
-                    <FormRegister />
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="content">
+        <div className="description-card">
+          <div className="description-card-container">
+            <h1>Learn to code by watching others</h1>
+            <p>
+              See how experienced developers solve problems in real-time.
+              Watching scripted tutorials is great, but understanding how
+              developers think is invaluable.
+            </p>
+          </div>
         </div>
-    )
-}
+        <div className="register-form-container">
+          <div className="try-it-free">
+            <strong>Try it free 7 days</strong> then $20/mo. thereafter
+          </div>
+          <FormRegister />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
